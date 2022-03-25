b6cedb1ad56d2d0f51a6726ca6a88efe5d11677d
makhabatabd <makhabat9898@gmail.com>
2022-03-25 11:18:35 +0600

    a few routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import Routing from "./Routing";
 function App() {
-  return <div>hello!</div>;
+  return (
+    <div>
+      <Routing />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Routing.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import ProductsList from "./components/ProductsList/ProductsList";
+import AdminPage from "./pages/AdminPage";
+import Error404 from "./pages/Error404";
+
+const Routing = () => {
+  let PUBLIC_ROUTES = [
+    {
+      link: "/",
+      element: <HomePage />,
+      id: 1,
+    },
+    {
+      link: "/products",
+      element: <ProductsList />,
+      id: 2,
+    },
+  ];
+  let ADMIN_ROUTES = [
+    {
+      link: "/admin",
+      element: <AdminPage />,
+      id: 1,
+    },
+  ];
+  return (
+    <BrowserRouter>
+      <Routes>
+        {PUBLIC_ROUTES.map((item) => (
+          <Route key={item.id} path={item.link} element={item.element} />
+        ))}
+        {ADMIN_ROUTES.map((item) => (
+          <Route key={item.id} path={item.link} element={item.element} />
+        ))}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routing;
